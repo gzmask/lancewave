8a89bf036508cac8d59b6bca9dc0c94606534b92
gzmask <gzmask@hotmail.com>
2011-09-07 22:15:04 +0000

    minor

--- lancewave.js
+++ lancewave.js
@@ -1,18 +1,12 @@
 (function() {
   var app, express;
-  express = require("express");
+  express = require('express');
   app = express.createServer();
-  app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.static(__dirname + "/public"));
-    app.set("views", __dirname + "/views");
-    app.set('view engine', 'coffee');
-    return app.register('.coffee', require('coffeekup').adapters.express);
-  });
-  app.get('/', function(req, res) {
-    return res.render('index', {
-      foo: 'bar'
-    });
+  app.register('.coffee', require('coffeekup'));
+  app.set('view engine', 'coffee');
+  app.use(express.static(__dirname + '/public'));
+  app.get('/', function(request, response) {
+    return response.render('index');
   });
   app.listen(3000);
   console.log("Express server started on port %s", app.address().port);
--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+coffee -c app.coffee
+mv app.js lancewave.js
 node_modules/forever/bin/forever start lancewave.js
