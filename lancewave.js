d2d4a0997da8ad06e62b1b6234ec9a93d13e1322
gzmask <gzmask@hotmail.com>
2011-09-07 14:42:40 -0600

    rename for forever

--- lancewave.js
+++ lancewave.js
@@ -5,7 +5,14 @@
   app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.static(__dirname + "/public"));
-    return app.set("views", __dirname + "/views");
+    app.set("views", __dirname + "/views");
+    app.set('view engine', 'coffee');
+    return app.register('.coffee', require('coffeekup').adapters.express);
+  });
+  app.get('/', function(req, res) {
+    return res.render('index', {
+      foo: 'bar'
+    });
   });
   app.listen(3000);
   console.log("Express server started on port %s", app.address().port);
