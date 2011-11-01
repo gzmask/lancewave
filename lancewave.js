e14af72ae30f38e91a677cda8f066739623cbcb2
gzmask <gzmask@hotmail.com>
2011-11-01 01:49:59 +0000

    remove coffeescript

--- lancewave.js
+++ lancewave.js
@@ -1,9 +1,17 @@
 (function() {
   var app, express;
   express = require('express');
+
+  //lazyboy
+  Model = require('LazyBoy');
+  Model.create_connection("lancewave");
+  Model.load('models');
+
   app = express.createServer();
-  app.register('.coffee', require('coffeekup'));
-  app.set('view engine', 'coffee');
+  app.set('views',__dirname+'/views');
+  app.set('view engine', 'ejs');
+  //app.register('.coffee', require('coffeekup'));
+  //app.set('view engine', 'coffee');
   app.use(express.static(__dirname + '/public'));
   app.get('/', function(request, response) {
     return response.render('index');
--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-coffee -c app.coffee
-mv app.js lancewave.js
 node_modules/forever/bin/forever start lancewave.js
