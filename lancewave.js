26c0c94a601981778797063f1bd8ac9a500f4640
gzmask <gzmask@hotmail.com>
2011-11-01 05:20:04 +0000

    lazy boy ORM layer added

--- lancewave.js
+++ lancewave.js
@@ -6,12 +6,20 @@
   Model = require('LazyBoy');
   Model.create_connection("lancewave");
   Model.load('models');
+  var User = Model('User');
+  var my_user = User.where("name","Jimi", function(err,users){
+    console.log(users);
+  });
+  //var my_user = User.create({name: "Jimi", surname: "Hendrix"});
+  /*my_user.save(function(err,saved_user){
+    console.log(saved_user);
+  });*/
+
 
   app = express.createServer();
   app.set('views',__dirname+'/views');
   app.set('view engine', 'ejs');
-  //app.register('.coffee', require('coffeekup'));
-  //app.set('view engine', 'coffee');
+
   app.use(express.static(__dirname + '/public'));
   app.get('/', function(request, response) {
     return response.render('index');
